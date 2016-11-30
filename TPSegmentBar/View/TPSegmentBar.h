32a829fa7bafeb6efb0c8954165781d2635b9f11
WPF <weipengfei@panda.tv>
2016-11-30 11:46:51 +0800

    调整目录结构

---
+++ TPSegmentBar/View/TPSegmentBar.h
@@ -0,0 +1,13 @@
+//
+//  TPSegmentBar.h
+//  TPSegmentBar
+//
+//  Created by WPF on 16/11/30.
+//  Copyright © 2016年 WPF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TPSegmentBar : UIView
+
+@end
---
+++ TPSegmentBar/View/TPSegmentBar.m
@@ -0,0 +1,21 @@
+//
+//  TPSegmentBar.m
+//  TPSegmentBar
+//
+//  Created by WPF on 16/11/30.
+//  Copyright © 2016年 WPF. All rights reserved.
+//
+
+#import "TPSegmentBar.h"
+
+@implementation TPSegmentBar
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
